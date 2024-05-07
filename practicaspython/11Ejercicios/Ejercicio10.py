0e2b4a9e397cb0222cc776a3f9ceaa94d1380282
Alexisrdrgz <rodriguez.alexis0404@gmail.com>
2024-05-06 19:34:01 -0700

    practicaspython

--- practicaspython/11Ejercicios/Ejercicio10.py
+++
@@ -1,7 +0,0 @@
-def emailRebanado():
-    correo = input("Ingresa tu dirección de correo electrónico: ").strip()
-    usuario = correo[:correo.index('@')]
-    dominio = correo[correo.index('@') + 1:]
-    print(f"Nombre de usuario es: {usuario} \n Y el dominio es: {dominio}")
-
-emailRebanado()
--- practicaspython/11Ejercicios/Ejercicio3.py
+++
@@ -1,6 +0,0 @@
-def c_Palabras(texto):
-    palabras = texto.split()
-    return len(palabras)
-
-texto_Usuario = input("Ingresa una oración: ")
-print(f"La oración tiene {c_Palabras(texto_Usuario)} palabras")
